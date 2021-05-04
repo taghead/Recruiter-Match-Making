911bedb670ad1a2ff9dc5bb89c3e7ebc48e4578c
Cameron Tavelli <s3668468@student.rmit.edu.au>
2021-05-04 21:42:55 +1000

    updated users/jobs

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,9 @@
-var firebaseConfig = {
-apiKey: "AIzaSyAUYfyiQEAm34NjF27EspaPfcLm5Gq4jBU",
-authDomain: "group-01-match-making-cosc2408.firebaseapp.com",
-projectId: "group-01-match-making-cosc2408",
-storageBucket: "group-01-match-making-cosc2408.appspot.com",
-messagingSenderId: "784245292062",
-appId: "1:784245292062:web:1595179e05c02b90c3020f",
-measurementId: "G-63R4C1NDKD"
-};
-firebase.initializeApp(firebaseConfig);
-// firebase.auth().useEmulator("http://localhost:9099");
-// firebase.firestore().useEmulator("localhost", 8080);
-// firebase.functions().useEmulator("localhost", 5001);
-
 document.addEventListener('DOMContentLoaded', function() { // Materialize Components
+
   var modals = document.querySelectorAll('.modal'); // Declare modal
-  M.Modal.init(modals, modalOptions); // Initialize modal
+  M.Modal.init(modals); // Initialize modal
+
   var items = document.querySelectorAll('.collapsible'); // Declare Collapsible
   M.Collapsible.init(items); // Initialize collapsible
+
 });
