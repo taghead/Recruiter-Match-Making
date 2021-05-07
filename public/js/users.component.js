98d75c3d0369ac73e177278df2263e39735f166d
Cameron Tavelli <s3668468@student.rmit.edu.au>
2021-05-08 00:18:43 +1000

    Reading skills from Firestore working

--- public/js/users.component.js
+++ public/js/users.component.js
@@ -5,6 +5,7 @@
     <script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-firestore.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-functions.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="js/skills.js"></script>
     <script src="js/index.js"></script>
     <script src="js/auth.helpers.js"></script>
     <script src="js/users.component.js"></script>
@@ -27,7 +28,6 @@ firebase.auth().onAuthStateChanged(function (user) {
           document.querySelector('#user-loc').value = details.location;
           document.querySelector('#user-bio').value = details.biography;
           document.querySelector('#user-exp').value = details.experience;
-          document.querySelector('#user-skills').value = details.skills;
 
           // undefined Checks
           if (document.querySelector('#user-title').innerHTML == 'undefined') {
@@ -48,31 +48,46 @@ firebase.auth().onAuthStateChanged(function (user) {
           if (document.querySelector('#user-exp').value == 'undefined') {
             document.querySelector('#user-exp').value = "";
           }
-          if (document.querySelector('#user-skills').value == 'undefined') {
-            document.querySelector('#user-skills').value = "";
+          
+          // SKILLS
+          // Set element
+          document.querySelector('#user-skills-input').value = details.skills;
+          // undefined check
+          if (document.querySelector('#user-skills-input').value != 'undefined') {
+            // Clear the input 
+            document.querySelector('#user-skills-input').value = "";
+            // Interate throught the array and print skills as M_Chips
+            for (let i = 0; i < details.skills.length; i++) { 
+              document.querySelector('#user-skills').M_Chips.addChip({
+                tag: details.skills[i],
+              });
+            }
+          } else { // IF undefined
+            document.querySelector('#user-skills-input').value = "";
           }
 
-        // Hide/Show elements
-        document.querySelector('.progress').style.display = "none"; // Hide loading bar
-        document.querySelector('.card').style.display = "block"; // Show profile
+          // Hide/Show elements
+          document.querySelector('.progress').style.display = "none"; // Hide loading bar
+          document.querySelector('.card').style.display = "block"; // Show profile
 
-        // updateUserProfile form
-        const updateUserProfile = document.querySelector('#update-user-profile');
-        updateUserProfile.addEventListener('submit', (e) => {
-          e.preventDefault();
-          firebase.firestore().collection('users').doc(doc.id).update({
-            // Update Details
-            name: document.querySelector('#user-name').value,
-            email: document.querySelector('#user-email').value,
-            dob: document.querySelector('#user-dob').value,
-            location: document.querySelector('#user-loc').value,
-            biography: document.querySelector('#user-bio').value,
-            experience: document.querySelector('#user-exp').value,
-            //skills: document.querySelector('#user-skills').value,
-          }).then(() => {
-            location.reload(); // Reload Page
-          }).catch(err => { console.log(err.message) });
-        });
+          // updateUserProfile form
+          const updateUserProfile = document.querySelector('#update-user-profile');
+          updateUserProfile.addEventListener('submit', (e) => {
+            e.preventDefault();
+            firebase.firestore().collection('users').doc(doc.id).update({
+              // Update Details
+              name: document.querySelector('#user-name').value,
+              email: document.querySelector('#user-email').value,
+              dob: document.querySelector('#user-dob').value,
+              location: document.querySelector('#user-loc').value,
+              biography: document.querySelector('#user-bio').value,
+              experience: document.querySelector('#user-exp').value,
+
+              
+            }).then(() => {
+              location.reload(); // Reload Page
+            }).catch(err => { console.log(err.message) });
+          });
         }
       })
     }, err => { 
