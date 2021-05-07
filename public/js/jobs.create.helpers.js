9ef28e13bfc76fbda5a08f632a5f926479f628a0
Andrew <taghead@taghead.xyz>
2021-05-07 17:25:37 +1000

    Feature complete

--- public/js/jobs.create.helpers.js
+++ public/js/jobs.create.helpers.js
@@ -14,10 +14,27 @@ function createJobListing(){
       const users =  firebase.firestore().collection("users").get().then((userDoc) => {
         const res = userDoc.forEach((userDoc) => {
           if (userDoc.data()['email'] == user.email){
-            firebase.firestore().collection('users').doc(userDoc.id).set({ listings: firebase.firestore.FieldValue.arrayUnion("OK") }, { merge: true });
+            let skillsLen = document.getElementById("skills").M_Chips.chipsData.length
+            let skills = [];
+            for ( let i=0; i<skillsLen;i++){
+              skills.push(document.getElementById("skills").M_Chips.chipsData[i]['tag'])
+            }
+            firebase.firestore().collection("listings").add({
+                jobName: document.getElementById("job-name").value,
+                description: document.getElementById("description").value,
+                skills: skills
+            })
+            .then(function(docRef) {
+                firebase.firestore().collection('users').doc(userDoc.id).set({ listings: firebase.firestore.FieldValue.arrayUnion(docRef.id) }, { merge: true });
+            })
           }
         })
       })
     }
   })
+  document.getElementById("modal-create").M_Modal.close()
+  setTimeout(() => {
+      document.getElementById("job-list").innerHTML = "";
+      updateJobList();
+    }, 2000);
 }
