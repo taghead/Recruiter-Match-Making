cae1bc68efb693fa7e9d6c0f913d30cd29c327a3
Andrew <taghead@taghead.xyz>
2021-05-07 23:22:47 +1000

    Reads chips

---
+++ public/js/jobs.edit.helpers.js
@@ -0,0 +1,38 @@
+document.getElementById('edit-job-open-modal').addEventListener('click', function(){
+  document.getElementById("job-edit-list").innerHTML = ''
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      const users =  firebase.firestore().collection("users").get().then((userDoc) => {
+        const res = userDoc.forEach((userDoc) => {
+          if (userDoc.data()['email'] == user.email){
+            for (let i=0; i<userDoc.data()['listings'].length;i++){
+              var docRef = firebase.firestore().collection("listings").doc(userDoc.data()['listings'][i]);
+              docRef.get().then((doc) => {
+                document.getElementById("job-edit-list").innerHTML += `
+                <option value="${userDoc.data()['listings'][i]}">${doc.data()['jobName']}</option>
+                `
+              })
+            }
+          }
+        })
+      })
+  }
+  })
+}, function(event){event.preventDefault()});
+
+document.getElementById('job-edit-list').onchange = function(){
+  var docRef = firebase.firestore().collection("listings").doc(document.getElementById("job-edit-list").value);
+  docRef.get().then((doc) => {
+    document.getElementById('edit-job-name').value = doc.data()['jobName']
+    document.getElementById('edit-description').value = doc.data()['description']
+    skillsEditLen = document.getElementById("skillsEdit").M_Chips.chipsData.length
+    for ( let i=0; i<skillsEditLen;i++){
+      document.getElementById("skillsEdit").M_Chips.deleteChip(0)
+    }
+    for ( i in doc.data()['skills']){
+      document.getElementById("skillsEdit").M_Chips.addChip({
+        tag: doc.data()['skills'][i]
+      });
+    }
+  })  
+}
