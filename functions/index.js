1317136a4244e17c6f9322e9aca77ae18db38a06
Andrew <taghead@taghead.xyz>
2021-05-08 03:50:43 +1000

    Match user button added

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ admin.initializeApp();
 // });
 
 // localhost:5001/group-01-match-making-co-78d4c/us-central1/matchUsers
-exports.matchUsers = functions.https.onRequest(async (req, res) => {
+exports.matchUsers = functions.https.onCall( async (data, context) => {
     const listings = await admin.firestore().collection("listings").get().then((listingDoc) => {
         const res = listingDoc.forEach((listingDoc) => {
             const users =  admin.firestore().collection("users").get().then((userDoc) => {
@@ -22,35 +22,38 @@ exports.matchUsers = functions.https.onRequest(async (req, res) => {
                 let list = []
 
                 const res = userDoc.forEach((userDoc) => {
+                    if (userDoc.data()['skills']){
 
-                    skillWanted = userDoc.data()['skills']
-                    skillAquire = listingDoc.data()['skills']
-                    skillsMatch = []
+                        skillWanted = userDoc.data()['skills']
+                        skillAquire = listingDoc.data()['skills']
+                        skillsMatch = []
 
-                    for ( i in skillWanted ){
-                        for ( j in skillAquire ){
-                            if ( skillWanted[i] == skillAquire[j] ){
-                                skillsMatch.push(skillAquire[j])
+                        for ( i in skillWanted ){
+                            for ( j in skillAquire ){
+                                if ( skillWanted[i] == skillAquire[j] ){
+                                    skillsMatch.push(skillAquire[j])
+                                }
                             }
                         }
-                    }
 
-                    var percentage = 0;
-                    percentage = (skillsMatch.length / skillWanted.length) * 100;
+                        var percentage = 0;
+                        percentage = (skillsMatch.length / skillWanted.length) * 100;
 
-                    if ( percentage >= 50 ) {
-                        console.log("\t\t\t\t\t\tMatch: " + percentage);
-                        list.push(userDoc.data()['email'])  
+                        if ( percentage >= 50 ) {
+                            console.log("\t\t\t\t\t\tMatch: " + percentage);
+                            list.push(userDoc.data()['email'])  
+                        }
                     }
                 })
                 let docRef = admin.firestore().collection("listings").doc(listingDoc.id);
                 docRef.update({
                     candidates: list
                 })
+
             })
         })
     })
-    res.send("Job listings have been matched with appropriate candidates")
+    //res.send("Job listings have been matched with appropriate candidates")
   });
 
 
@@ -101,91 +104,91 @@ exports.getJobCandidates = functions.https.onCall( async (data, context) => {
   });
 
 // // localhost:5001/group-01-match-making-co-78d4c/us-central1/testData
-exports.testData = functions.https.onRequest(async (req, res) => {
-    // Test user 1
-    var docData = {
-        email: "test@test.com",
-        skills: ["Dancing", "Piano", "Vocals", "Charismatic"],
-    };
-    admin.firestore().collection("users").doc("1").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+// exports.testData = functions.https.onRequest(async (req, res) => {
+//     // Test user 1
+//     var docData = {
+//         email: "test@test.com",
+//         skills: ["Dancing", "Piano", "Vocals", "Charismatic"],
+//     };
+//     admin.firestore().collection("users").doc("1").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test user 2
-    var docData = {
-        email: "test2@test2.com",
-        skills: ["Jumping", "Climbing", "Vaulting", "Hugging"],
-    };
-    admin.firestore().collection("users").doc("2").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test user 2
+//     var docData = {
+//         email: "test2@test2.com",
+//         skills: ["Jumping", "Climbing", "Vaulting", "Hugging"],
+//     };
+//     admin.firestore().collection("users").doc("2").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test user 3
-    var docData = {
-        email: "test3@test3.com",
-        skills: ["Javascript", "Angular", "NodeJs", "Deno"],
-    };
-    admin.firestore().collection("users").doc("3").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test user 3
+//     var docData = {
+//         email: "test3@test3.com",
+//         skills: ["Javascript", "Angular", "NodeJs", "Deno"],
+//     };
+//     admin.firestore().collection("users").doc("3").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test user 4
-    var docData = {
-        email: "test4@test4.com",
-        skills: ["Packing"],
-    };
-    admin.firestore().collection("users").doc("4").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test user 4
+//     var docData = {
+//         email: "test4@test4.com",
+//         skills: ["Packing"],
+//     };
+//     admin.firestore().collection("users").doc("4").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test listing 1
-    var docData = {
-        jobName: "Running friends",
-        skills: ["Running", "Climbing", "Vaulting"],
-        candidates: []
-    };
-    admin.firestore().collection("listings").doc("1").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test listing 1
+//     var docData = {
+//         jobName: "Running friends",
+//         skills: ["Running", "Climbing", "Vaulting"],
+//         candidates: []
+//     };
+//     admin.firestore().collection("listings").doc("1").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test listing 2
-    var docData = {
-        jobName: "A Very Active Programmer",
-        skills: ["Running", "Climbing", "Vaulting", "Javascript", "Angular", "NodeJs", "Deno"],
-        candidates: []
-    };
-    admin.firestore().collection("listings").doc("2").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test listing 2
+//     var docData = {
+//         jobName: "A Very Active Programmer",
+//         skills: ["Running", "Climbing", "Vaulting", "Javascript", "Angular", "NodeJs", "Deno"],
+//         candidates: []
+//     };
+//     admin.firestore().collection("listings").doc("2").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test listing 3
-    var docData = {
-        jobName: "Active Programmer - Meblourne Central Safeway",
-        skills: ["Running", "NodeJs", "Deno"],
-        candidates: []
-    };
-    admin.firestore().collection("listings").doc("3").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test listing 3
+//     var docData = {
+//         jobName: "Active Programmer - Meblourne Central Safeway",
+//         skills: ["Running", "NodeJs", "Deno"],
+//         candidates: []
+//     };
+//     admin.firestore().collection("listings").doc("3").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
     
-    // Test listing 4
-    var docData = {
-        jobName: "General Labour - Box Hill",
-        skills: ["RF Scanning", "Mathematics", "Hard Labour"],
-        candidates: []
-    };
-    admin.firestore().collection("listings").doc("4").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
+//     // Test listing 4
+//     var docData = {
+//         jobName: "General Labour - Box Hill",
+//         skills: ["RF Scanning", "Mathematics", "Hard Labour"],
+//         candidates: []
+//     };
+//     admin.firestore().collection("listings").doc("4").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
 
-    // Test listing 5
-    var docData = {
-        jobName: "Packing",
-        skills: ["Packing"],
-        candidates: []
-    };
-    admin.firestore().collection("listings").doc("5").set(docData).then(() => {
-        console.log("Document successfully written!");
-    });
-    res.send("Test data added")
-})
+//     // Test listing 5
+//     var docData = {
+//         jobName: "Packing",
+//         skills: ["Packing"],
+//         candidates: []
+//     };
+//     admin.firestore().collection("listings").doc("5").set(docData).then(() => {
+//         console.log("Document successfully written!");
+//     });
+//     res.send("Test data added")
+// })
