f22bb3ced33961908f25869f5f2b086856f3f7e7
Andrew Chander <andrewchander@gmail.com>
2021-08-23 19:39:18 +1000

    Added test describe and account details modal click

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -1,28 +1,40 @@
 const puppeteer = require('puppeteer');
 
-('Recruiter Sign-Up', async () => {
-    const browser = await puppeteer.launch({
-                headless: false,
-                slowMo: 10
-            });
+const browserArgs = {
+    headless: false,
+    slowMo: 10
+}
 
-    const page = await browser.newPage();
-
-    await page.goto('http://localhost:5000');
-    await page.click('[data-target="modal-login"]');
-    await page.type('input[type="email"]', "recruiter@company.com");
-    await page.type('input[type="password"]', "123123TA123123");
-    await page.select('select[id="role"', 'employer');
-    // await page.click('button[id="sign-up"]');
-    await page.click('button[id="login"]');
-    page.waitForSelector('div[class="collapsible-header"]').then(() => {
-        const loggedInAs = page.$eval('a[id="account-details"]', el => el.textContent);
-        expect(loggedInAs).toBe('recruiter@company.com');
-    })
-    .catch((err) => {
-        console.log(err);
+describe('Recruiter', () => {
+    beforeAll(async () => {
+        await page.goto('http://localhost:5000');
     });
 
-    // await page.screenshot({ path: 'example.png' });
-    // await browser.close();
-  })();
+    it('recruiter should be signed up', async () => {
+
+        const browser = await puppeteer.launch(browserArgs);
+
+        const page = await browser.newPage();
+
+        await page.goto('http://localhost:5000');
+        await page.click('[data-target="modal-login"]');
+        await page.type('input[type="email"]', "recruiter@company.com");
+        await page.type('input[type="password"]', "123123TA123123");
+        await page.select('select[id="role"', 'employer');
+        // await page.click('button[id="sign-up"]');
+        
+        await page.click('button[id="login"]');
+
+        page
+            .waitForSelector('a[id="account-details-icon"]')
+            .then(() => page.click('a[id="account-details-icon"]'));
+
+        page
+            .waitForSelector('h4[id="user-title"]', 50)
+            .then(() => {
+                const loggedInAs = page.$eval('h4[id="user-title"]', el => console.log(el));
+                expect(loggedInAs).toBe('recruiter@company.com');
+            })
+
+    });
+});
