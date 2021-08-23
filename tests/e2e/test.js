eb48004c958d719ad8669063b933ebd4b9946e4c
Andrew Chander <andrewchander@gmail.com>
2021-08-24 08:40:16 +1000

    Added querySelector to recruiter sign up

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -17,12 +17,13 @@ const roleDropdown = 'select[id="role"';
 const signUpBtn = 'button[id="sign-up"]';
 const signUpBtnRecruiterRole = 'employer';
 const accountDetailsBtn = 'a[id="account-details-icon"]'
+const accountDetailsEmail = '[id="user-title"]';
 
 describe('Recruiter', () => {
     beforeAll(async () => {
     });
     
-    it('recruiter should be signed up', async () => {
+    it('should be signed up', async () => {
         const browser = await puppeteer.launch(browserArgs);
         const page = await browser.newPage();
         await page.goto('http://localhost:5000');
@@ -38,10 +39,10 @@ describe('Recruiter', () => {
             .then(() => page.click(accountDetailsBtn));
 
         await page.waitForFunction(
-            `document.getElementById('user-title').innerText.includes("recruiter@company.com")`,
+            `document.querySelector('${accountDetailsEmail}').innerText.includes('${recruiterEmail}')`,
           );
 
-        const loggedInAs = await page.$eval('[id="user-title"]', e => e.innerHTML);
+        const loggedInAs = await page.$eval(accountDetailsEmail, e => e.innerHTML);
         expect(loggedInAs).toBe(recruiterEmail);
 
         browser.close();
