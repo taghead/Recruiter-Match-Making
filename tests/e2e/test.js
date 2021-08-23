1042674a9a04079110965f75e1c9b350ab1f7c37
Andrew Chander <andrewchander@gmail.com>
2021-08-23 18:54:02 +1000

    Added jest to dev dependencies

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+('Recruiter Sign-Up', async () => {
     const browser = await puppeteer.launch({
                 headless: false,
                 slowMo: 10
@@ -13,7 +13,15 @@ const puppeteer = require('puppeteer');
     await page.type('input[type="email"]', "recruiter@company.com");
     await page.type('input[type="password"]', "123123TA123123");
     await page.select('select[id="role"', 'employer');
-    await page.click('button[id="sign-up"]');
+    // await page.click('button[id="sign-up"]');
+    await page.click('button[id="login"]');
+    page.waitForSelector('div[class="collapsible-header"]').then(() => {
+        const loggedInAs = page.$eval('a[id="account-details"]', el => el.textContent);
+        expect(loggedInAs).toBe('recruiter@company.com');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
     // await page.screenshot({ path: 'example.png' });
     // await browser.close();
